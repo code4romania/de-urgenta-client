0b6fa1fa35db61b7800f497d95287ab64ad85f03
Ciprian Platica <platica.ciprian@gmail.com>
2021-04-03 20:10:41 +0300

    Use semantic tags

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,21 +1,28 @@
 <template>
-  <div class="relative text-center">
-    <div class="relative lg:w-1/2 m-auto z-10">
-      <div class="text-4xl md:text-5xl font-bold">
-        Aici ar veni un header catchy pentru download singura aplicatie care
-      </div>
-      <div class="text-xl mt-8">
-        Aici ar fi un alt text care sa imputerniceasca ce zice textul de mai sus
-        si sa complementeze call to action-ul pentru downloading-ul aplicatiei.
-        pwp.
-      </div>
-      <div class="flex justify-center mt-8 space-x-5">
-        <a href="#"><LogoDownloadApple /></a>
-        <a href="#"><LogoDownloadAndroid /></a>
-      </div>
-    </div>
-    <div class="absolute z-0 left-0 right-0 top-full -mt-16">
-      <ImageWave class="w-auto h-32 sm:w-full sm:h-auto" />
+  <div class="text-center lg:w-1/2 m-auto">
+    <h1 class="text-4xl text-gray-800 font-bold md:text-5xl">
+      {{ title }}
+    </h1>
+    <p class="text-xl mt-8">
+      {{ description }}
+    </p>
+    <div class="flex justify-center space-x-5 mt-8">
+      <a href="#"><img src="~assets/logo-download-ios.svg" /></a>
+      <a href="#"><img src="~assets/logo-download-android.svg" /></a>
     </div>
   </div>
 </template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
