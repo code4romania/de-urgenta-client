6379d860cf6d51e4ced109eb168100f7e1d8e11d
Ciprian Platica <platica.ciprian@gmail.com>
2021-04-03 22:24:24 +0300

    Use props and semantic tags

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,57 +1,53 @@
 <template>
   <div
-    class="flex flex-col-reverse md:flex-row shadow-md rounded-xl p-5 lg:p-10"
+    class="flex flex-col-reverse md:flex-row shadow-md rounded-xl p-5 lg:p-10 bg-white"
   >
     <div>
-      <div class="text-4xl font-bold">Transfer funds world-wide</div>
-      <div class="mt-2.5 text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima
-        sequi recusandae, porro maiores officia assumenda aliquam laborum ab
-        aliquid veritatis impedit odit adipisci optio iste blanditiis facere.
-        Totam, velit.
-      </div>
-      <div class="space-y-9 mt-10">
+      <h2 class="text-4xl font-bold">{{ title }}</h2>
+      <p class="mt-2.5 text-gray-500">
+        {{ description }}
+      </p>
+      <div v-for="(item, index) in list" :key="index" class="space-y-9 mt-10">
         <div class="flex">
           <div>
-            <div class="bg-red-500 rounded p-2 mr-5"><IconWorld /></div>
-          </div>
-          <div>
-            <div class="text-2xl font-bold">Competitive exchange rates</div>
-            <div class="text-gray-500 pt-1.5">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
-              impedit perferendis suscipit eaque, iste dolor cupiditate
-              blanditiis ratione.
+            <div class="bg-red-500 rounded p-2 mr-5 w-8 h-8">
+              <img :src="require(`~/assets/` + item.icon)" />
             </div>
           </div>
-        </div>
-        <div class="flex">
           <div>
-            <div class="bg-red-500 rounded p-2 mr-5"><IconScale /></div>
-          </div>
-          <div>
-            <div class="text-2xl font-bold">No hidden fees</div>
-            <div class="text-gray-500 pt-1.5">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
-              impedit perferendis suscipit eaque, iste dolor cupiditate
-              blanditiis ratione.
-            </div>
-          </div>
-        </div>
-        <div class="flex">
-          <div>
-            <div class="bg-red-500 rounded p-2 mr-5"><IconLightning /></div>
-          </div>
-          <div>
-            <div class="text-2xl font-bold">Transfers are instant</div>
-            <div class="text-gray-500 pt-1.5">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
-              impedit perferendis suscipit eaque, iste dolor cupiditate
-              blanditiis ratione.
-            </div>
+            <h3 class="text-2xl font-bold">{{ item.title }}</h3>
+            <p class="text-gray-500 pt-1.5">
+              {{ item.content }}
+            </p>
           </div>
         </div>
       </div>
     </div>
-    <div><ImageFeature class="m-auto md:ml-20" /></div>
+    <div class="flex-shrink-0">
+      <img class="m-auto md:ml-20" :src="require(`~/assets/` + image)" />
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    list: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,40 @@
 <template>
   <div>
     <NavigationBar />
-    <section class="container mx-auto px-5 mt-10 md:mt-16">
-      <Hero
-        title="Aici ar veni un header catchy pentru download singura aplicatie care"
-        description="Aici ar fi un alt text care sa imputerniceasca ce zice textul de mai sus
-        si sa complementeze call to action-ul pentru downloading-ul aplicatiei.
-        pwp."
-      />
-    </section>
-    <Feature />
+    <main class="bg-gray-50">
+      <section class="container mx-auto px-5 pt-10 md:pt-16">
+        <Hero
+          title="Aici ar veni un header catchy pentru download singura aplicatie care"
+          description="Aici ar fi un alt text care sa imputerniceasca ce zice textul de mai sus si sa complementeze call to action-ul pentru downloading-ul aplicatiei. pwp."
+        />
+      </section>
+      <section class="container mx-auto px-5 mt-10 md:mt-16">
+        <Feature
+          title="Transfer funds world-wide"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima sequi recusandae, porro maiores officia assumenda aliquam laborum ab aliquid veritatis impedit odit adipisci optio iste blanditiis facere. Totam, velit."
+          image="image-feature.svg"
+          :list="[
+            {
+              icon: 'icon-world.svg',
+              title: 'Competitive exchange rates',
+              content:
+                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+            },
+            {
+              icon: 'icon-scale.svg',
+              title: 'No hidden fees',
+              content:
+                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+            },
+            {
+              icon: 'icon-lightning.svg',
+              title: 'Transfers are instant',
+              content:
+                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+            },
+          ]"
+        />
+      </section>
+    </main>
   </div>
 </template>
