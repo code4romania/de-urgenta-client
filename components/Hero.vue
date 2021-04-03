38aff979cc8202aa3b14414053a6388675acb878
Ciprian Platica <platica.ciprian@gmail.com>
2021-04-03 03:49:55 +0300

    Add Hero component

---
+++ components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="relative text-center">
+    <div class="relative lg:w-1/2 m-auto z-10">
+      <div class="text-4xl md:text-5xl font-bold">
+        Aici ar veni un header catchy pentru download singura aplicatie care
+      </div>
+      <div class="text-xl mt-8">
+        Aici ar fi un alt text care sa imputerniceasca ce zice textul de mai sus
+        si sa complementeze call to action-ul pentru downloading-ul aplicatiei.
+        pwp.
+      </div>
+      <div class="flex justify-center mt-8 space-x-5">
+        <a href="#"><LogoDownloadApple /></a>
+        <a href="#"><LogoDownloadAndroid /></a>
+      </div>
+    </div>
+    <div class="absolute z-0 left-0 right-0 top-full -mt-16">
+      <ImageWave class="w-auto h-32 sm:w-full sm:h-auto" />
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <NavigationBar />
+    <Hero />
   </div>
 </template>
