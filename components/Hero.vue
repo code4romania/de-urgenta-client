fb64d2a64f093f8c60098e12f4e4f05b5f3a28ea
Cipick <platica.ciprian@gmail.com>
2021-04-03 20:13:44 +0300

    Add Hero component (#13)
    * Add Hero component
    
    * Use semantic tags

---
+++ components/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="text-center lg:w-1/2 m-auto">
+    <h1 class="text-4xl text-gray-800 font-bold md:text-5xl">
+      {{ title }}
+    </h1>
+    <p class="text-xl mt-8">
+      {{ description }}
+    </p>
+    <div class="flex justify-center space-x-5 mt-8">
+      <a href="#"><img src="~assets/logo-download-ios.svg" /></a>
+      <a href="#"><img src="~assets/logo-download-android.svg" /></a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <NavigationBar />
+    <section class="container mx-auto px-5 mt-10 md:mt-16">
+      <Hero
+        title="Aici ar veni un header catchy pentru download singura aplicatie care"
+        description="Aici ar fi un alt text care sa imputerniceasca ce zice textul de mai sus
+        si sa complementeze call to action-ul pentru downloading-ul aplicatiei.
+        pwp."
+      />
+    </section>
   </div>
 </template>
