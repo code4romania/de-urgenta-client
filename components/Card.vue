bd9d72a3b5d8b231bfbcaa88e2e215ed75248146
Cipick <platica.ciprian@gmail.com>
2021-04-05 00:57:03 +0300

    Add Card component (#10)
    * Add Card component
    
    * Update tags
    
    * Use props
    
    * Use image instead of component

---
+++ components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="flex flex-col lg:flex-row bg-white shadow-md rounded-xl p-5 lg:p-10"
+  >
+    <div>
+      <h2 class="text-4xl font-bold">
+        {{ title }}
+      </h2>
+      <p class="mt-7 text-gray-500">
+        {{ description }}
+      </p>
+      <div class="mt-7">
+        <a
+          href="#"
+          class="inline-block bg-red-500 hover:bg-red-600 text-white p-2.5 pl-12 pr-12 rounded-md"
+        >
+          <span>{{ button }}</span>
+          <img src="~/assets/icon-arrow.svg" class="inline-block" />
+        </a>
+      </div>
+    </div>
+    <div class="flex-shrink-0 lg:pt-0">
+      <img src="~/assets/image-creators.svg" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    button: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
