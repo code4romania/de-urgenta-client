a6a90c256a1a2ede0680cfcbe309379b3ff19d27
Ciprian Platica <platica.ciprian@gmail.com>
2021-04-22 10:36:19 +0300

    Add strapi CMS

---
+++ de-urgenta-backend-web/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'de874deb8ad951cb802b5031631db363'),
+    },
+  },
+});
